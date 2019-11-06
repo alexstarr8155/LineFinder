c5edbaa2889a44b137b3e66d24221427e33034f0
starr <starralex@live.com>
2019-11-05 21:04:48 -0500

    More tests

--- app/src/main/java/com/example/lineapp/MainActivity.java
+++ app/src/main/java/com/example/lineapp/MainActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    println("hello")
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("hello");
+
     }
 }
