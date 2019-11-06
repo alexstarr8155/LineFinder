0fe6f4222ff1a4f2a01609deb82fd7d5ca6d9b40
Landson <guotl321@gmail.com>
2019-11-05 21:01:04 -0500

    test

--- app/src/main/java/com/example/lineapp/MainActivity.java
+++ app/src/main/java/com/example/lineapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    println("hello")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
