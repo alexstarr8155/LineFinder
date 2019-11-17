05ab8a20e989aa391bf7fb41b0328566a7eb7bad
Dhruv Mittal <57159970+dhmi19@users.noreply.github.com>
2019-11-17 00:39:53 -0500

    Created a recyclable card list view using a tutorial
    The animation is pretty bad and we should probably change it
    
    The card layout is wrong, we need relevant text fields and images should not take up such a big portion of the card

--- app/src/main/java/com/example/lineapp/MainActivity.java
+++ app/src/main/java/com/example/lineapp/MainActivity.java
@@ -3,17 +3,41 @@ package com.example.lineapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listview_layout);
+        mListView = (ListView) findViewById(R.id.listView);
 
-        System.out.println("hello");
-        System.out.println("Dhruv");
-        int h = 6;
+        ArrayList<Card> list = new ArrayList<>();
+        list.add(new Card("@drawable/arizona_dessert", "Arizona Desert"));
+        list.add(new Card("drawable://" + R.drawable.bamf1, "Bamf"));
+        list.add(new Card("drawable://" + R.drawable.colorado_mountains, "Colorado Mountains"));
+        list.add(new Card("drawable://" + R.drawable.cork, "Cork"));
+        list.add(new Card("drawable://" + R.drawable.davenport_california, "DavenPort California"));
+        list.add(new Card("drawable://" + R.drawable.denmark_austrailia, "Denmark Austrailia"));
+        list.add(new Card("drawable://" + R.drawable.foggy_iceland, "Foggy Iceland"));
+        list.add(new Card("drawable://" + R.drawable.havasu_falls, "Havasu Falls"));
+        list.add(new Card("drawable://" + R.drawable.hawaii_rainforest, "Hawaii RainForest"));
+        list.add(new Card("drawable://" + R.drawable.newfoundland_ice, "NewFoundLand Ice"));
+        list.add(new Card("drawable://" + R.drawable.oregon_greens, "Oregon Greens"));
+        list.add(new Card("drawable://" + R.drawable.stc_pic, "STC Tim Hortons"));
+        list.add(new Card("drawable://" + R.drawable.yosemite, "Yosemite"));
+
+
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_main, list);
+        mListView.setAdapter(adapter);
 
     }
 }
