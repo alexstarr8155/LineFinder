7a0fd675741a2fc64505358f01bd7b9411c37e47
a258chan <a258chan@uwaterloo.ca>
2019-12-01 16:31:26 -0500

    gps incomplete

--- app/src/main/java/com/example/lineapp/MainActivity.java
+++ app/src/main/java/com/example/lineapp/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.core.app.NotificationCompat;
 import android.view.View;
 import android.content.Context;
 import android.widget.ListView;
-
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,6 +18,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    // GPS
+    private LocationManager locationManager=null;
+    private LocationListener locationListener=null;
 
     private static final String TAG = "MainActivity";
 
@@ -43,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
         Collections.sort(PlaceList.places);
         CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_main, PlaceList.places);
         mListView.setAdapter(adapter);
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager
+                .GPS_PROVIDER, 5000, 10,locationListener);
+        locationListener = new MyLocationListener();
     }
 
     public void sendNotification(View view) {
